9be988bd50e832abbbfd880400f7c540a69bfc1d
Kiranpal121 <kiran121pal@gmail.com>
2024-07-10 09:49:24 +0530

    Day3

---
+++ Day3/src/Encapsulation/Encapsulationdemo.java
@@ -0,0 +1,23 @@
+package Encapsulation;
+
+public class Encapsulationdemo {
+
+public static void main(String[] args) {
+
+		
+		//object creation
+		OopsConceptDemo obj= new OopsConceptDemo();
+		
+		obj.setName("Shubham");
+		obj.setAge(25);
+		obj.setSerilaNum(1001);
+		
+		System.out.println(obj.getAge());
+		System.out.println(obj.getName());
+		System.out.println(obj.getSerilaNum());
+		
+		System.out.println(obj);
+		
+		
+	}
+}
---
+++ Day3/src/Encapsulation/OopsConceptDemo.java
@@ -0,0 +1,31 @@
+package Encapsulation;
+
+public class OopsConceptDemo {
+
+	private int serilaNum;
+	private String name;
+	private int age;
+	public int getSerilaNum() {
+		return serilaNum;
+	}
+	public void setSerilaNum(int serilaNum) {
+		this.serilaNum = serilaNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "OopsConceptDemo [serilaNum=" + serilaNum + ", name=" + name + ", age=" + age + "]";
+	}
+}
